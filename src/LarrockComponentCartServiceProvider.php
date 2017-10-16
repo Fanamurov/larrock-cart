08670d3ba8f6272410f138feb8a0757c2319ac64
Alexandr Fanamurov <fanamura@gmail.com>
2017-10-16 14:27:46 +1000

    update

--- src/LarrockComponentCartServiceProvider.php
+++ src/LarrockComponentCartServiceProvider.php
@@ -20,6 +20,8 @@ class LarrockComponentCartServiceProvider extends ServiceProvider
         $this->publishes([
             __DIR__.'/views' => base_path('resources/views/vendor/larrock')
         ]);
+
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     /**
@@ -33,17 +35,5 @@ class LarrockComponentCartServiceProvider extends ServiceProvider
             $class = config('larrock.components.cart', CartComponent::class);
             return new $class;
         });
-
-        $timestamp = date('Y_m_d_His', time());
-        $timestamp_after = date('Y_m_d_His', time()+10);
-        $migrations = [];
-        if ( !class_exists('CreateCartTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_create_cart_table.php'] = database_path('migrations/'.$timestamp.'_create_cart_table.php');
-        }
-        if ( !class_exists('AddForeignKeysToCartTable')){
-            $migrations[__DIR__.'/database/migrations/0000_00_00_000000_add_foreign_keys_to_cart_table.php'] = database_path('migrations/'.$timestamp_after.'_add_foreign_keys_to_cart_table.php');
-        }
-
-        $this->publishes($migrations, 'migrations');
     }
 }
